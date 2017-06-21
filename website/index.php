fd5b7784b21387913a16e091c3e6d6417e453f70
allexramon <alexramonsg@gmail.com>
2017-06-20 21:27:39 -0300

    Aula 88 - Slim Framework
    Trabalhando com ROTAS utilzando o Slim Framework 2.0

--- website/index.php
+++ website/index.php
@@ -1,5 +1,20 @@
 <?php 
 
-phpinfo();
+require_once("vendor/autoload.php");
+
+//app onde vai ser criado as rotas
+$app = new \Slim\Slim();
+
+$app->get('/', function(){
+	echo json_encode(array(
+		'date'=>date("Y-m-d H:i:s")
+		));
+});
+
+$app->get('/hello/:name', function ($name) {
+    echo "Hello, " . $name;
+});
+
+$app->run();
 
  ?>
